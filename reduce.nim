672465726312de21cb4ed33119c3e8fd1ea96d22
wjl12 <bill@bill-lee.biz>
2022-11-07 08:36:49 -0500

    Added documentation

--- reduce.nim
+++ reduce.nim
@@ -1,2 +1,7 @@
 func reduce(φ1, φ2, τ: float64): float64 =
+    ## Reduces two surface into one using Gaussian reduction
+    ## φ1, φ2: power of surface 1 and 2, respectively
+    ## τ: index adjusted distance from surface 1 to surface 2
+    ##    τ = t/n
+    ## returns: power of the reduced surface
     result = φ1 + φ2 - τ*φ1*φ2
